0948be291c8f1b890f2f641df3e633abf8cdd3e4
MulmVerbot <70117614+MulmVerbot@users.noreply.github.com>
2024-06-21 18:12:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from tkinter import *
+import tkinter as tk
+import PIL
 
 
 class Fullscreen_Window:
 
     def __init__(self):
-        self.tk = Tk()
-        #self.tk.attributes('-zoomed', True)  # This just maximizes it so we can see the window. It's nothing to do with fullscreen.
+        self.tk = tk()
         self.frame = Frame(self.tk)
         self.frame.pack()
         self.state = False
         self.tk.bind("<F11>", self.toggle_fullscreen)
         self.tk.bind("<Escape>", self.end_fullscreen)
+
+
+    def Programm_Start(self):
+        print("start")
+
+    def GUI_Spawn_Menu(self):
+        print("gui spawn menü")
+        img = tk.PhotoImage(Image.open('dein_bild.png'))
+
+
 
     def toggle_fullscreen(self, event=None):
         self.state = not self.state  # Just toggling the boolean
