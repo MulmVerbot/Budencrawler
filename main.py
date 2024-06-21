cac3843fd3ee1b6f4714b4e84fadb95a09d2e76b
MulmVerbot <70117614+MulmVerbot@users.noreply.github.com>
2024-06-21 20:59:09 +0200

    check nich was sein Problem ist

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import tkinter as tk
 import PIL
 
 
-class Fullscreen_Window:
-
-    def __init__(self):
-        self.tk = tk()
-        self.frame = Frame(self.tk)
-        self.frame.pack()
+class Budencrawler:
+    def __init__(self, master):
+        self.master = master
         self.state = False
-        self.tk.bind("<F11>", self.toggle_fullscreen)
-        self.tk.bind("<Escape>", self.end_fullscreen)
+        root.title("Budencrawler Alpha")
+        root.bind("<F11>", self.toggle_fullscreen)
+        root.bind("<Escape>", self.end_fullscreen)
+        self.Bild = tk.PhotoImage(file="2.png")
+        #self.Bild_L = tk.Label(root, image=self.Bild)
 
 
     def Programm_Start(self):
@@ -18,9 +18,6 @@ class Fullscreen_Window:
 
     def GUI_Spawn_Menu(self):
         print("gui spawn menü")
-        img = tk.PhotoImage(Image.open('dein_bild.png'))
-
-
 
     def toggle_fullscreen(self, event=None):
         self.state = not self.state  # Just toggling the boolean
@@ -33,5 +30,18 @@ class Fullscreen_Window:
         return "break"
 
 if __name__ == '__main__':
-    w = Fullscreen_Window()
-    w.tk.mainloop()
+    root = tk.Tk()
+    width = 420
+    height = 420
+
+    def mittig_fenster(root, width, height):
+        fenster_breite = root.winfo_screenwidth()
+        fenster_höhe = root.winfo_screenheight()
+        x = (fenster_breite - width) // 2
+        y = (fenster_höhe - height) // 2
+        root.geometry(f"{width}x{height}+{x}+{y}")
+        root.resizable(False,False)
+
+    mittig_fenster(root, width, height)
+    Budencrawler = Budencrawler(root)
+    root.mainloop()
